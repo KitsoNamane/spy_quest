183ee43b9f1a3422b24208209431a7e460c66cd6
pedrogedion7 <infiniti.kitso@gmail.com>
2015-12-18 00:03:43 +0200

    remove dashes from scene text add palitable styling to home.html

--- map.py
+++ map.py
@@ -43,45 +43,39 @@ class Scene(object):
 
 the_pit = Scene('the pit',
 """
--------------------------------------------------------------------------------
-mission briefing:
+MISSION BRIEFING:
 hello agent T-19...this is Diana, it seems we're in need of your skills!
 
-------------------------------------------
-#infiltrate the webster Mansion
----they have a social function tonight and
----i'v prepared a cover for you...
-#find and secure a disk (ATLANTIS-virus)
-------------------------------------------
+*infiltrate the webster Mansion, they have a social function tonight and
+ i'v prepared a cover for you...
+*find and secure a disk (ATLANTIS-virus)
 
 our client would like this to be as clean as possible so don't leave a mess!,
 we don't RIO all over again do we...see you soon!!!
--------------------------------------------------------------------------------
 
--------------------------------------------------------------------------------
 tools: lock-pick, flash-drive, tight-rope...
 
 you arrive with your envoy at the main entrance but the door is only as far as
 you can go...
->>>choose your cover???
--------------------------------------------------------------------------------
+
+
+choose your cover???
 """
 )
 
 
 the_front_door = Scene('the front door',
 """
--------------------------------------------------------------------------------
 you arrive as the body-guard of Mr Del Piero, but you can only go as far as the
 main entrace of the mansion...
->>>what's the plan???
--------------------------------------------------------------------------------
+
+
+what's the plan???
 """
 )
 
 the_kitchen = Scene('the kitchen',
 """
--------------------------------------------------------------------------------
 with this disguise you sneak in the kitchen...steam soaring, stove hissing and
 everybody passing around, they hardly notice you at all...you go about
 inspecting each and every corner of the room you then notice familiar room,
@@ -89,72 +83,65 @@ the cold room! suddenly you get an idea...
 a few minutes later you emerge out og the cold room as Josh, the a waiter!
 just then the head Chef takes a load on you, pointing and telling you where you
 should be, he then gives you an order, its for a VIP at the master bedroom
->>>are you going to take it?
--------------------------------------------------------------------------------
+
+
+are you going to take it?
 """
 )
 
 the_lounge = Scene('the lounge',
 """
--------------------------------------------------------------------------------
 you pose as a steward, with special interest at the main table you serve them
 wine, listening in to their conversation one thing that caught your attention:
 
--------------------------------------------------------------------------------
-///Webster: (with a cigar in his hand) I'm telling you she is a beauty, I took
-/////////// her for a runthe other day...it was fenominal
-///Cage: (hahaha..) that's what you said about Dinah...now look at what trouble
-//////// she's gotten you into...(all laughing)
+Webster: (with a cigar in his hand) I'm telling you she is a beauty, I took
+         her for a runthe other day...it was fenominal
+Cage: (hahaha..) that's what you said about Dinah...now look at what trouble
+      she's gotten you into...(all laughing)
 
------------------------------------------------------------
 deep in your thoughts:
 {find Dinah, she must know one or two of his secrets}
 you then go to the elevator...there is a guard there:
 
--------------------------------------------------------------
-///Guard: hey you cant go up there..
-///You: Oh special order for Mrs Webster..
-///Guard: which Mrs Webster?
-///You: the, boss's escort, look i don't get paid a lot and i need this job so
-/////// unless you want both of us loosing our jobs...!
-///Guard: oh we don't want that do we, go on...master bedroom,
+Guard: hey you cant go up there..
+You: Oh special order for Mrs Webster..
+Guard: which Mrs Webster?
+You: the, boss's escort, look i don't get paid a lot and i need this job so
+     unless you want both of us loosing our jobs...!
+Guard: oh we don't want that do we, go on...master bedroom,
 
--------------------------------------------------------------------------------
 you are now in the elevator, check time:
 ...2105hrs... hm!!! not bad but time is running out...
->>>where do you want to go now???
--------------------------------------------------------------------------------
+
+
+where do you want to go now???
 """
 )
 
 the_study = Scene('the study',
 """
--------------------------------------------------------------------------------
 you are in the study, no one is there, you start to look around hoping to get
 lucky...you first look in the drawers, you find nothing interesting except a
 small speck of shine under some documents...its the key to the Farrari...you
 take it and start to look around again, then you soon notice an old mural...
 it proves to had hidden some secrets, its the safe it appears to have an old
 combination lock about 3 digits
--------------------------------------------------------------------------------
 """
 )
 
 the_bedroom = Scene('the bedroom',
 """
--------------------------------------------------------------------------------
 you enter the rooom, its candle lit, music playing softly in the background
 and rose petals everywhere. You find Dinah on the bed waiting for you!!!
->>>how are you going to get her to talk?!?
--------------------------------------------------------------------------------
+
+
+how are you going to get her to talk?!?
 """
 )
 
 the_security_room = Scene('the security room', """
--------------------------------------------------------------------------------
 you enter security room to cover your tracks, there is no one here so the
 computer is all yours fornow...Hurry!!!
--------------------------------------------------------------------------------
 """
 )
 
