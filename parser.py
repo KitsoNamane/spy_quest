df2e7ebe2e51a12d6719d2c714904d76176001d2
kitsoGideon <infiniti.kitso@gmail.com>
2016-05-08 17:11:59 +0200

    make a branch for experimental emplementation

--- parser.py
+++ parser.py
@@ -5,14 +5,18 @@ class ParserError(Exception):
     pass
 
 class Sentence(object):
-    def __init__(self, subject, verb, object):
+    def __init__(self, subject, negates, verb, object):
         # remember we take ('noun', 'princess') tuples and convert them
         self.subject = subject[1]
         self.verb = verb[1]
         self.object = object[1]
+        self.negates = negates
 
     def get_sentence(self):
-        self.sentence = ' '.join([self.subject, self.verb, self.object])
+        if self.negates == None:
+            self.sentence = ' '.join([self.subject, self.verb, self.object])
+        else:
+            self.sentence = 'game over'
         return self.sentence
 
 def peek(word_list):
@@ -41,12 +45,20 @@ def skip(word_list, word_type):
 
 def parse_verb(word_list):
     skip(word_list, 'stop')
+    print(word_list)
 
     if peek(word_list) == 'verb':
         return match(word_list, 'verb')
     else:
         raise ParserError("Expected a verb next.")
 
+def parse_negates(word_list):
+    skip(word_list, 'stop')
+
+    if peek(word_list) == 'negatives':
+        return match(word_list, 'negatives')
+    else:
+        return None
 
 def parse_object(word_list):
     skip(word_list, 'stop')
@@ -61,10 +73,11 @@ def parse_object(word_list):
 
 
 def parse_subject(word_list, subj):
+    negate = parse_negates(word_list)
     verb = parse_verb(word_list)
     obj = parse_object(word_list)
 
-    return Sentence(subj, verb, obj)
+    return Sentence(subj, negate, verb, obj)
 
 
 def parse_sentence(word_list):
@@ -75,7 +88,10 @@ def parse_sentence(word_list):
     if start == 'noun':
         subj = match(word_list, 'noun')
         return parse_subject(word_list, subj)
-    elif start == 'verb':
+    elif start == 'negatives':
+        # assume the subject is the player then
+        return parse_subject(word_list, ('noun', 'player'))
+    elif start == 'verb' and len(word_list) != 0:
         # assume the subject is the player then
         return parse_subject(word_list, ('noun', 'player'))
     else:
