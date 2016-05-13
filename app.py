3cb6cc731f50d55ac796a86dd75e74c8b99eb46c
kitsoGideon <infiniti.kitso@gmail.com>
2016-05-13 11:04:20 +0200

    build url's eg. /play/the pit, /play/kitchen

--- app.py
+++ app.py
@@ -19,18 +19,25 @@ def home_page():
     scene = session['scene.name']
     return render_template("home.html", scene=scene)
 
-@app.route('/play', methods=['GET', 'POST'])
-def play():
+@app.route('/play/<name>', methods=['GET', 'POST'])
+def play_game(name):
     if session['scene.name'] and request.method == 'POST':
         previous_scene = game_engine.get_scene(session['scene.name'])
         session_name = game_engine.handle_scene(request.form['action'])
         if session_name not in previous_scene.legal_paths:
             flash("you cant go there from this scene, try again.......")
             session['scene.name'] = session['scene.name']
-            return redirect(url_for('play'))
+            print("cow")
+            print(session['scene.name'])
+            print("cow")
+            return redirect(url_for('play_game', name=session['scene.name']))
         else:
             session['scene.name'] = session_name
-            return redirect(url_for('play'))
+            print("cow")
+            print(session['scene.name'])
+            print("cow")
+            return redirect(url_for('play_game', name=session['scene.name']))
+    print(session['scene.name'])
     current_scene = game_engine.get_scene(session['scene.name'])
     return render_template('play.html', scene=current_scene)
 
